ec1e578b975ba56ed1b7382f3aef3390d43b5c7b
sahilkhot1708 <vmcreation2026@gmail.com>
2026-08-10 23:02:23 -0700

    Update app to use environment variable for port

--- hospital-management/app.py
+++ hospital-management/app.py
@@ -393,5 +393,7 @@ def admin_stats():
     }), 200
 
 
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
